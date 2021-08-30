80d162e1b90cfaca74d399bba168767e5ce13885
Deniks <rezanovich,denis@gmail.com>
2021-08-30 15:18:48 +0100

    init

---
+++ client/src/OtherPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export const OtherPage = () => {
+  return (
+    <div>
+      Other Page !! <Link to="/">Go back home</Link>{' '}
+    </div>
+  );
+};
